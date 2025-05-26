c32c09cb4860443c32f159050d3da8bfec06a08a
tkimhong <91110603+tkimhong@users.noreply.github.com>
2025-05-26 16:56:38 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+//REG API
+app.post("/registration", (req, res) => {
+  res.send("<html><body>INSIDE REGISTRATION API..</body></html>");
+});
+
+app.post("/login", (req, res) => {
+  res.send("<html><body>INSIDE LOGIN API..</body></html>");
+});
+
+//SEARCH API
+app.get("/search", (req, res) => {
+  res.send("<html><body>INSIDE SEARCH API..</body></html>");
+});
+
+//UPDATE API
+app.put("/updateprofile", (req, res) => {
+  res.send("<html><body>INSIDE UPDATE PROFILE API..</body></html>");
+});
+
+//DELETE API
+app.delete("/deleteuser", (req, res) => {
+  res.send("<html><body>INSIDE DELETE API..</body></html>");
+});
+
+// START THE EXPRESS SERVER. 5000 is the PORT NUMBER
+app.listen(5000, () => console.log("EXPRESS Server Started at Port No: 5000"));
